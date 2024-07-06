4a849482d7bcc29dbd532dc9e7fc47de962fd09a
Giovane Santos <49699679+giovane358@users.noreply.github.com>
2024-07-05 22:11:21 -0300

    API BASE - STOCKBAY

---
+++ src/main/java/br/com/stockbay/userstockbay/UserstockbayApplication.java
@@ -0,0 +1,13 @@
+package br.com.stockbay.userstockbay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserstockbayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserstockbayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/stockbay/userstockbay/UserstockbayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.stockbay.userstockbay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserstockbayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
