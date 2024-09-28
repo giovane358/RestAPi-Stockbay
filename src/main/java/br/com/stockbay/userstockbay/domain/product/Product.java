34e9d2e4581c4f2acc59031a104ea9940ccc538b
giovane358 <givanesilva.etec@gmail.com>
2024-09-28 00:59:41 -0300

    att product

--- src/main/java/br/com/stockbay/userstockbay/domain/product/Product.java
+++ src/main/java/br/com/stockbay/userstockbay/domain/product/Product.java
@@ -21,6 +21,46 @@ public class Product {
     private Number price_in_cents;
     private Boolean active;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDtCompra() {
+        return dtCompra;
+    }
+
+    public void setDtCompra(Date dtCompra) {
+        this.dtCompra = dtCompra;
+    }
+
+    public Number getPrice_in_cents() {
+        return price_in_cents;
+    }
+
+    public void setPrice_in_cents(Number price_in_cents) {
+        this.price_in_cents = price_in_cents;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
     public Product(ProductRequest data) {
         this.name = data.name();
         this.price_in_cents = data.price_in_cents();
